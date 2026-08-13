5d2779fe8c70f1c768de304b2c6a527e35d7c44a
sofiepatoor <90647683+sofiepatoor@users.noreply.github.com>
2026-08-13 21:54:43 +0200

    add: books collection (mock data)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,28 @@
 // Import the glob loader and astro utilities
-import { glob } from "astro/loaders";
+import { glob, file } from "astro/loaders";
 import { defineCollection } from "astro:content";
+
 // Zod (= schema declaration and validation library for TypeScript)
 import { z } from "astro/zod";
+
 // Define a `loader` and `schema` for each collection
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
-    schema: z.object({
-      title: z.string(),
-      created_date: z.date(),
-      updated_date: z.date(),
-      short_description: z.string(),
-    })
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    created_date: z.date(),
+    updated_date: z.date(),
+    short_description: z.string(),
+  }),
 });
 
-export const collections = { blog };
+const books = defineCollection({
+  loader: file("src/media/books.json"),
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    date_read: z.string(),
+  }),
+});
+
+export const collections = { blog, books };
